d477bd5663494636a6f298e859196fa04a0e150f
Joan <detchenoujoan@gmail.com>
2025-01-04 19:57:26 +0100

    update

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ echo "Connexion à EC2 et déploiement..."
 
 # Sauvegarder l'image de développement dans le fichier env
 echo IMAGE_NAME=$DEV_IMAGE >> .secure_files/env.dev
+echo ECR_URI=$ECR_URI >> .secure_files/env.dev
 
 # Copier les fichiers nécessaires sur l'instance EC2
 scp -i .secure_files/trinity-dev-key-pair.pem .secure_files/env.dev ubuntu@$EC2_PUBLIC_IP:/home/ubuntu/.env
@@ -13,6 +14,7 @@ scp -i .secure_files/trinity-dev-key-pair.pem docker-compose.yml ubuntu@$EC2_PUB
 # Exécution des commandes à distance via SSH
 ssh -i .secure_files/trinity-dev-key-pair.pem ubuntu@$EC2_PUBLIC_IP << 'EOF'
   source /home/ubuntu/.env
+  aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin $ECR_URI
   docker-compose -f /home/ubuntu/docker-compose.yml down
   docker-compose -f /home/ubuntu/docker-compose.yml pull
   docker-compose -f /home/ubuntu/docker-compose.yml up -d
