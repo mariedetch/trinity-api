05cdca4f6d81e4e50f5a280bc1b753df32a29799
Joan <detchenoujoan@gmail.com>
2025-01-04 13:07:59 +0100

    update

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,11 @@
 # Connexion à l'instance EC2 et téléversement des fichiers
 echo "Connexion à EC2 et déploiement..."
 
-# Téléchargement des variables d'environnement
-gitlab-ci env.dev download --file .env
-
 # Sauvegarder l'image de développement dans le fichier env
 echo IMAGE_NAME=$DEV_IMAGE >> env
 
 # Copier les fichiers nécessaires sur l'instance EC2
-scp -i .secure_files/trinity-dev-key-pair.pem .env ubuntu@$EC2_PUBLIC_IP:/home/ubuntu/.env
+scp -i .secure_files/trinity-dev-key-pair.pem .secure_files/env.dev ubuntu@$EC2_PUBLIC_IP:/home/ubuntu/.env
 scp -i .secure_files/trinity-dev-key-pair.pem docker-compose.yml ubuntu@$EC2_PUBLIC_IP:/home/ubuntu/docker-compose.yml
 
 # Exécution des commandes à distance via SSH
